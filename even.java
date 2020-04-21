0b02ae96873ec04481b038737de3272ba082f372
chaithanya-124 <64065230+chaithanya-124@users.noreply.github.com>
2020-04-21 00:25:14 -0700

    Add files via upload

---
+++ even.java
@@ -0,0 +1,15 @@
+class A{
+	public static void main (String args[]){
+		int num;
+		int n=50;
+		for (num = 0; num < n; num++) {
+			if(num%2==0){
+		if ( num%3==0 && num%5==0){
+        
+         	
+			
+				System.out.println(num);
+			}
+		}}
+	}}
+	
---
+++ prod.java
@@ -0,0 +1,17 @@
+class A{
+	public static void main (String args[]){
+		int i=1;
+		int j=5;
+		int n=100;
+		int sum=0;
+		sum=i*j;
+		for(i=1;i<n;i++){
+			for(j=2;j<n;j++){
+				sum=i*j;
+			}
+  System.out.println(sum);
+}
+	
+}
+}
+
