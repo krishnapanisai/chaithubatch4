85b17c83ce991e9f8912257705ef21609b3fdc4f
chaithanya-124 <64065230+chaithanya-124@users.noreply.github.com>
2020-04-22 23:18:52 -0700

    Add files via upload

---
+++ one.java
@@ -0,0 +1,70 @@
+class A{
+	public static void main(String args[]){
+	int n=1;
+	for(int i=1;i<=3;i+=1){
+	for(int j=1;j<=3;j+=1){
+System.out.print(n+"");
+n++;
+
+	}if(i!=3){
+		System.out.println();
+	}
+	System.out.println();
+	System.out.println();
+	
+	}
+	}
+	}
+
+/* i=1,1<3 true
+ j=1, 1<3 true
+ "1"
+ j++
+ n=2
+ j=2,2<3 true
+ "12"
+ J++
+ n=3
+ j=3 3=3 true
+ "123"
+ j++
+ n=4
+ n!=3 true
+ newline
+
+ i=2,1<3 true
+ j=1, 1<3 true
+ "4"
+ j++
+ n=5
+ j=2,2<3 true
+ "45"
+ J++
+ n=6
+ j=3 3=3 true
+ "456"
+ j++
+ n=6
+ n!=3 true
+ newline
+ i=1,1<3 true
+ j=1, 1<3 true
+ "7"
+ j++
+ n=8
+ j=2,2<3 true
+ "78"
+ J++
+ n=9
+ j=3 3=3 true
+ "789"
+ j++
+ n=9
+ n!=3 true
+ newline*/
+ 
+
+ 
+
+
+
---
+++ three.java
@@ -0,0 +1,22 @@
+class A{
+	public static void main(String args[]){
+	String n="$#$";
+	String p="#$#";
+
+	for(int i=1;i<=1;i++){
+	
+System.out.println(n);
+
+	}
+	System.out.println(p);
+	System.out.println(n);
+	
+	
+	}
+	}
+/*i=1 i<1
+ prints"n"
+ i=1 i<1
+ prints"p"
+ i=1 i<1
+ prints"n"*/
